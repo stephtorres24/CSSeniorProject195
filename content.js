de00babeb6fe5badc8d8f8c8b292b118dbfeada4
stephtorres24 <s_torres13@u.pacific.edu>
2023-11-15 15:49:29 -0800

    handling safety tool tips toggle

--- content.js
+++ content.js
@@ -1,16 +1,14 @@
-// Listen for page load event
+//listen for page load event
 window.addEventListener('load', function () {
-    // Check if this is a new tab or a regular webpage
+
     if (window.location.href === 'chrome://newtab/') {
-      // Handle new tab behavior, e.g., display a message
       const newTabPageMessage = document.createElement('div');
       newTabPageMessage.textContent = 'Welcome to Netiquette New Tab!';
       document.body.appendChild(newTabPageMessage);
     } else {
-      // Handle regular webpage behavior
       const body = document.body;
   
-      // Add a tooltip to the "New Tab" button
+      //add a tooltip to the "New Tab" button
       const newTabButton = document.querySelector('#new-tab-button');
       if (newTabButton) {
         const tooltip = document.createElement('div');
@@ -18,7 +16,7 @@ window.addEventListener('load', function () {
         newTabButton.appendChild(tooltip);
       }
   
-      // Add a tooltip to the scroll wheel
+      //add a tooltip to the scroll wheel
       window.addEventListener('wheel', function (event) {
         if (event.deltaY > 0) {
           const tooltip = document.createElement('div');
@@ -27,14 +25,14 @@ window.addEventListener('load', function () {
           tooltip.style.top = '10px';
           tooltip.style.left = '10px';
           document.body.appendChild(tooltip);
-          // Remove the tooltip after a few seconds
+          //remove the tooltip after a few seconds
           setTimeout(() => {
             tooltip.remove();
           }, 3000);
         }
       });
   
-      // Add best practice tooltips for password prompts
+      //add best practice tooltips for password prompts
       const passwordPrompts = document.querySelectorAll('input[type="password"]');
       passwordPrompts.forEach(function (prompt) {
         prompt.addEventListener('focus', function () {
@@ -42,7 +40,7 @@ window.addEventListener('load', function () {
         });
       });
   
-      // Warn about advertisements when hovering over links
+      //warn about advertisements when hovering over links
       const links = document.querySelectorAll('a');
       links.forEach(function (link) {
         link.addEventListener('mouseover', function () {
@@ -53,4 +51,4 @@ window.addEventListener('load', function () {
       });
     }
   });
-  //test
+  
