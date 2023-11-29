fa6f706116f8df58ae3bc5940214bdd0bf6a1445
Lucas Blaydes <l_blaydes@u.pacific.edu>
2023-11-29 04:07:13 -0800

    updated ad warning method

--- content.js
+++ content.js
@@ -5,12 +5,12 @@ function addShortcutPopup(){
   elem.id = 'shortcutPopup';
   elem.style.cssText = 'position:fixed;top:120px;right:20px;width:10em;height:2.6em;text-align:center;font-size: 1.5em;font-family:Tahoma, sans-serif;font-weight:600;line-height:1.1em;display:flex;align-items:center;justify-content:center;opacity:0.7;z-index:100;border-radius:15px;border: 2px solid #000000;background:#ADD8E6;-webkit-text-stroke-width: 0.2px;-webkit-text-stroke-color: white;';
   
-  elem.style.backgroundColor = chrome.storage.sync.get("themeColor",function(res) {
+  elem.style.backgroundColor = localStorage.getItem("themeColor",function(res) {
     console.log(res[key])
     });
   elem.classList.add("netiquette");
     while(!shortcutPoppedUp){
-        var pickTip = Math.floor(Math.random() * 4);
+        var pickTip = Math.floor(Math.random() * 5);
         switch(pickTip) {
             case 0:
               elem.innerHTML = 'New Tab<br>Ctrl+T';
@@ -20,15 +20,15 @@ function addShortcutPopup(){
               elem.innerHTML = 'New Window<br>Ctrl+N';
               shortcutPoppedUp = true;
               break;
-            case 1:
+            case 2:
               elem.innerHTML = 'New Bookmark<br>Ctrl+D';
               shortcutPoppedUp = true;
               break;
-            case 2:
+            case 3:
               elem.innerHTML = 'Close Tab<br>Ctrl+W';
               shortcutPoppedUp = true;
               break;
-            case 3:
+            case 4:
               elem.innerHTML = 'Close Window<br>Ctrl+Shift+W';
               shortcutPoppedUp = true;
               break;
@@ -43,6 +43,8 @@ function addShortcutPopup(){
 
 //listen for page load event
 window.addEventListener('load', function () {
+    localStorage.setItem("themeColor", "#F2EFC7");
+
     addShortcutPopup();
     // Check if this is a new tab or a regular webpage
 
@@ -63,7 +65,7 @@ window.addEventListener('load', function () {
         tooltip.textContent = 'Scroll: Arrow keys → ↑ ↓ ←';
         tooltip.id = 'scrollTooltip';
         tooltip.style.cssText = 'position:fixed;bottom:80px;right:20px;width:10em;height:2.6em;text-align:center;font-size: 1.5em;font-family:Tahoma, sans-serif;font-weight:600;line-height:1.1em;display:flex;align-items:center;justify-content:center;opacity:0.08;z-index:100;border-radius:15px;border: 2px solid #000000;background:#ADD8E6;-webkit-text-stroke-width: 0.2px;-webkit-text-stroke-color: white;';
-        tooltip.style.backgroundColor = chrome.storage.sync.get("themeColor",function(res) {
+        tooltip.style.backgroundColor = localStorage.getItem("themeColor",function(res) {
           console.log(res[key])
           });
         tooltip.classList.add("netiquette");
@@ -86,10 +88,31 @@ window.addEventListener('load', function () {
   
       // Warn about advertisements when hovering over links
       const links = document.querySelectorAll('a');
+      const adWarn = document.createElement('div');
+      adWarn.textContent = 'Link may be an Ad';
+      adWarn.id = 'adWarning';
+      adWarn.style.cssText = 'position:fixed;top:40%;right:40%;width:10em;height:2.6em;text-align:center;font-size: 1.5em;font-family:Tahoma, sans-serif;font-weight:600;line-height:1.1em;display:flex;align-items:center;justify-content:center;opacity:0.7;z-index:100;border-radius:15px;border: 2px solid #000000;background:#ADD8E6;-webkit-text-stroke-width: 0.2px;-webkit-text-stroke-color: white;';
+      adWarn.classList.add("netiquette");
+
       links.forEach(function (link) {
         link.addEventListener('mouseover', function () {
+          
           if (link.textContent.toLowerCase().includes('ad')) {
-            alert('Warning: This link may lead to an advertisement.');
+            var rect = link.getBoundingClientRect();
+            var xPos = rect.left;
+            var yPos = rect.top;
+            var linkColor = link.style.color;
+            link.style.color = "#FF0000"
+            adWarn.style.backgroundColor = localStorage.getItem("themeColor");
+            adWarn.style.left = xPos;
+            adWarn.style.top = yPos;
+            document.body.appendChild(adWarn);
+      
+            // Remove the ad warning after a few seconds
+            setTimeout(() => {
+              adWarn.remove();
+              link.style.color = linkColor;
+            }, 1500);
           }
         });
       });
